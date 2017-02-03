35f5699c392cd38f768fcb82ff3f80e1f0fa50d3
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2017-02-03 21:55:49 +0800

    I forget this

--- install_python.sh
+++ install_python.sh
@@ -7,11 +7,11 @@ wget http://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
 tar zxfv Python-3.6.0.tgz
 find ~/python -type d | xargs chmod 0755
 cd Python-3.6.0
-./configure --prefix=$HOME/python # ./configure --enable-optimizations ?
+./configure --prefix=$HOME/python # --enable-optimizations ?
 make
 make install
 
 cd
 echo \#Python >> .bashrc
-echo export PATH=$HOME/python/Python-3.6.0/:\$PATH >> .bashrc
+echo export PATH=$HOME/python/bin/:\$PATH >> .bashrc
 
